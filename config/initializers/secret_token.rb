6336866bdae2ae69e75d7af6cabf121a2cb02abf
divine13 <dubedivine@gmail.com>
2013-09-22 20:12:55 +0200

    all testing tools are ready to be used

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeketeeApp::Application.config.secret_key_base = '2af2bbafa3bb1b7ea336d1dc03c7274d1dc365670eedef17c65b48feff5ac9873c11c92cc928c94267d84be3ad8bc01f29b8f049c534c380dcf34f87ab02671b'
