2e7a51957837fa9a1b605144012697251470d6bf
divine13 <dubedivine@gmail.com>
2013-09-23 23:01:41 +0200

    yeah added heavy commenting and some DRY code! and also helping a lost user nice

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,33 +1,37 @@
 class ProjectsController < ApplicationController
 
-	def index #CRUD ALWAYS FOLLOWS CRUD STEPS Dry code!
+     #heavily commented just to make sure everything i am on right ._path
+
+    before_filter :find_project, only: [:show, :edit, :update, :destroy] #DRY! 100%
+
+	def index    #CRUD. ALWAYS FOLLOW CRUD STEPS. DRY code!, nice...
 		@project = Project.all
 	end
-	def new 
-		@project = Project.new
+	def new    #this method passes the newly created object to the create method 
+		@project = Project.new #it carries the params to the create  method
 	end
-	def create 
-		@project = Project.new(project_params)
-		if @project.save
+
+	def create        #it process those newly created objects 
+		@project = Project.new(project_params) #it injects those objects into the DB
+		if @project.save #true or false . true! it must or it will blow 
 		redirect_to @project
-		flash[:notice] = "project hase been created"
+		flash[:notice] = "project has been created"
 	   else
-	   	render 'new'
+	   	render 'new' #it blows to this  
 	   	end
 	end
 
     def show
-    	@project = find_project
+    	#the before_filter in inserts  @project = Project.find(params[:id]) here
+    	#before this method is executed
     end
 
     def edit 
-    	@project = find_project
     end
 
     def update
-    	@project = find_project
     	if(@project.update_attributes(project_params))
-    	flash[:notice] = "project succesfully updated"
+    	flash[:notice] = "project has been updated"
     	redirect_to @project
     else
     	flash[:alert] = "project has not been update because:"
@@ -36,18 +40,22 @@ class ProjectsController < ApplicationController
     end
 
     def destroy
-    	@project = find_project
     	@project.destroy
     	flash[:alert] = "#{@project.name} project has been deleted"
     	redirect_to projects_path
     end
 
 
-    #rails 4 requirements for a better
+    #rails 4 requirements for better security
 	 def project_params
       params.require(:project).permit(:name)
     end
-    def find_project
-    	Project.find(params[:id])
+private
+    def find_project #the magic happens here
+    	@project = Project.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+    	flash[:alert] = "Sorry!, The page you are looking for is not available"
+    	redirect_to root_path
+
     end
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe ProjectsController do
-	
+	it "should redirect when to home when page not there" do
+		get :show, id: "someotherpage"
+		response.should redirect_to(root_path)
+		alert_msg = "Sorry!, The page you are looking for is not available"
+		flash[:alert].should eql(alert_msg)
+	  
+	end
 end
