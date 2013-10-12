381d2baebcc87f1314b1ca2183be41236f619911
divine13 <dubedivine@gmail.com>
2013-10-12 17:42:36 +0200

    i have seeded an admin user and normal user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+admin_user = User.create(email: "superdivine@dube.org", password: "verylongpasscode..")
+
+admin_user.admin = true
+admin_user.save
+
+user = User.create(email: "normaldivine@ticketee.org", password: "password")
+user.save
+
+Project.create(name: "ticketee beta")
