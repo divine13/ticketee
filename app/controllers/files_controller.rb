29ac4f6d62ccc381907446eac5223939d9da8318
divine13 <dubedivine@gmail.com>
2013-11-25 15:15:16 +0200

    refactor 2

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -7,8 +7,8 @@ class FilesController < ApplicationController
 		render partial: "files/form", locals: {number: params[:number].to_i, asset: asset}
 	end 
 	def show
-		asset = Asset.find(params[:id]) 
-		if(can?(:view, asset.ticket.project))
+		asset = Asset.find(params[:id])
+		if(can?(:view, asset.ticket.project) || current_user.admin? == true)#could have left out the true part but ahhhm.... 
 		send_file asset.asset.path, filename: asset.asset_file_name, 
 		content_type: asset.asset_content_type
          else
